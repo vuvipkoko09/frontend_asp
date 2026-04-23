049f087f9c805df4c38c0d25bfad4c7bbfa55862
Vu Nguyen <vu.nguyen@example.com>
2026-04-23 23:18:36 +0700

    mô tả những gì bạn vừa sửa

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Link chuẩn của Somee
-  baseURL: 'https://anhvu-asp.somee.com/api', 
+  baseURL: 'http://anhvu-asp.somee.com/api', 
   headers: { 'Content-Type': 'application/json' }
 });
 
